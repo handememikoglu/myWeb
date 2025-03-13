628af726134b04aa95b51a25f8f112f55522b89a
Hande Eylül Memikoğlu <handememikoglu@gmail.com>
2025-03-13 20:18:38 +0300

    dosya yapısı ve componentler oluşturuldu

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import Header from "../components/Header";
+import App from "../App";
+import HomePage from "../components/HomePage";
+
+export default function MainLayout(){
+    const [page,setPage] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const handlePopState = () => setPage(window.location.pathname);
+
+        window.addEventListener('popstate',handlePopState);
+        return () => window.removeEventListener('popstate',handlePopState);
+
+    },[])
+
+    const navigate = (path) => {
+        window.history.pushState({},"", path);
+        setPage(path);
+    }
+
+    return(
+        <div>
+            <Header navigate={navigate}/>
+            <div>
+                <App>
+                    {page === "/home" && <HomePage navigate={navigate} />}
+                </App>
+            </div>
+        </div>
+    )
+}
